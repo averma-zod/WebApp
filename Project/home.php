7c6ed3ecfd698f75e3b6b5b9ef4c3f6b914515bc
shreyanshgairola <shreyanshgairola@gmail.com>
2019-01-26 00:56:54 +0530

    Changes to be committed: ~

--- Project/home.php
+++ Project/home.php
@@ -5,20 +5,37 @@
 	<link rel="stylesheet" type="text/css" href="homestyle.css">
 </head>
 <body>
-    <h1 align="center">S.A Taxi Service</h1>
+	<div class="heading">
+        <h1 align="center">S.A Taxi Service</h1>
+    </div>
 
     <div class="header">
     	<table>
-    		<th><button class="headerbtn">Book</button></th>
-    		<th><button class="headerbtn">Book</button></th>
-    		<th><button class="headerbtn">Book</button></th>
+    		<th><button class="headerbtn">Home</button></th>
+    		<th><select class="headerdrpbtn" name="Category">
+    			   <option value="Abstract">Abstract</option>
+    			   <option value="Light Trails">Light Trails</option>
+    			   <option value="Wild Life">Wild Life</option>
+    		</select></th>
+
     		<th width="90%"></th>
-    		<th><select> name="Setting">
-    			   <option value="Setting">Setting</option>
-    			   <option value="">Setting</option>
-    			   <option value="Log Out">Log Out</option>
+
+    		<th><input class="search" type="text" name="search" placeholder="Photographer"></th>
+    		<th><button class="headerbtn">Search</button></th>
+    		<th><select class="headerdrpbtn" name="Setting">
+    			   <option value="Profile">My Profile</option>
+    			   <option value="ActSet">Account Settings</option>
+    			   <option value="LogOut">Log Out</option>
     		</select></th>
     	</table>
+    </div>
+    <div class="imagebody" align="center">
+       <div class="images" align="center">
+    	   <img class="image" style="width: 300px; height: 300px;" src="abc.jfif">
+    	   <p>Name:</p>
+    	   <p>Photographer:</p>
+    	   <p>Category:</p>
+       </div>
     </div>
 </body>
 </html>
