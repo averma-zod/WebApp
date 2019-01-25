557d6813e33d5830fcabc16b19cdfa90db7d9cb7
Shreyansh Gairola <37464984+ShreyanshGairola@users.noreply.github.com>
2019-01-25 22:39:16 +0530

    Add files via upload
    Project Folder

---
+++ Project/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="homestyle.css">
+</head>
+<body>
+    <h1 align="center">S.A Taxi Service</h1>
+
+    <div class="header">
+    	<table>
+    		<th><button class="headerbtn">Book</button></th>
+    		<th><button class="headerbtn">Book</button></th>
+    		<th><button class="headerbtn">Book</button></th>
+    		<th width="90%"></th>
+    		<th><select> name="Setting">
+    			   <option value="Setting">Setting</option>
+    			   <option value="">Setting</option>
+    			   <option value="Log Out">Log Out</option>
+    		</select></th>
+    	</table>
+    </div>
+</body>
+</html>
