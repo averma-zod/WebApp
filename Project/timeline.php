426f9c7f026db7de3f1ce04a8b54b86a3615606c
thanosbhai <avakashverma98@gmail.com>
2019-01-27 02:33:43 +0530

    timeline added

---
+++ Project/timeline.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head >
+	<title>Timeline</title>
+	<style>
+		h2 {
+			background-color: black;
+			margin-bottom: 0px;
+			border: 2px solid grey;
+			border-collapse: collapse;
+		}
+		body{
+			background-color: black;
+		}
+	</style>
+	<link rel="stylesheet" type="text/css" href="timeline.css"></link>
+</head>
+<body>
+		<img class="profile" src="abc.JFIF" alt="User">
+	
+	<h2 align="center" ><font color="white">UserName</font></h2>
+	<table  class="panel">
+		<tr>
+			<td></td>
+			<th align="left" width="8%">
+				<button class="set" style="border:1px solid grey;">Your photos</font></button>
+			</th>
+			<th align="left" width="8%">
+				<button class="set" style="border:1px solid grey;">Photos liked</font>
+			</th>
+			<th width="84%" align="right">
+				<select name="Setting" style="padding: 10px;color:white; background-color: black">
+				<option value="">Settings</option>
+				<option>account setting</option>
+				<option>log out</option>
+			</select>
+			</th>
+		</tr>
+	</table>
+	<img class="midline" src="u.PNG" alt="new">
+	<img class="midline" src="l.jpg" alt="User">
+	<img class="midline" src="abc.JFIF" alt="User">
+
+</body>
+</html>
